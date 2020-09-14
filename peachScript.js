f9af10cb9d199db9053e96010705bcc3c3ac4785
Brenna <brennamacquarrie@gmail.com>
2020-09-13 22:41:41 -0600

    reduce method new and working on other scroll options

--- peachScript.js
+++ peachScript.js
@@ -15,6 +15,22 @@ pricklyPeach.hamburger = function() {
     });
 }
 
+// function to scroll????
+
+// pricklyPeach.scrollTo = function(id) {
+//     console.log("scroll");
+
+//     if ($(id).length) {
+//         var getOffset = $(id).offset().top;
+//         var targetDistance = 50;
+//         $('html,body').animate({
+//             scrollTop: getOffset - targetDistance
+//         }, 500);
+//     }
+// }
+
+
+
 //function to send points into hogs obj
 pricklyPeach.hogPoints = function () {
     $(`.addPoints`).on(`submit`, function (e) {
@@ -24,17 +40,19 @@ pricklyPeach.hogPoints = function () {
         pricklyPeach.hogs[answer] += 1;
         
 
-        // this is where i got stuck. I wanted to stop it from scrolling but I think I'd need to build a new function to get the buttons to scroll. I want to take some time this week to learn ho wto do this properly so i can try it next time.
+        // this is where i got stuck. I wanted to stop it from scrolling but I think I'd need to build a new function to get the buttons to scroll differently. I want to take some time this week to learn how to do this properly so i can try it next time.
         
         // if (!answer) {
             // wanted to change this attribute on only the current form, but i'd have to toggle it...
-        //     $(this[".buttonSteez input"]).attr("onclick", "");
+
             // this throws an alert when the user hasn't picked an option, but it still scrolls to next page.
         //     swal({
         //         title: "please pick an answer!",
         //         button: "okie"
         //     });
-        // } 
+        // } else {
+            //pricklyPeach.scrollTo();
+        // }
             
     });
 }
@@ -45,19 +63,10 @@ pricklyPeach.displayHog = function() {
     $(`#done`).on(`submit`, function(e) {
         e.preventDefault();
 
-        let winningScore = 0;
-        let chosenHog = "";
+        const chosenHog = Object.keys(pricklyPeach.hogs).reduce((a, b) =>
+            pricklyPeach.hogs[a] > pricklyPeach.hogs[b] ? a : b
+        );
 
-        for (let hog in pricklyPeach.hogs) {
-            const score = pricklyPeach.hogs[hog];
-            
-            if (score == winningScore) {
-                chosenHog = hog;
-            } else if (score > winningScore) {
-                winningScore = score;
-                chosenHog = hog;   
-            } 
-        }
         $(`.finalPage img[id=${chosenHog}]`).toggleClass(`showMe`);
 
         $(`.resultHog`).text(`${chosenHog}`);
@@ -84,6 +93,7 @@ pricklyPeach.reset = function() {
 // BUILD INIT FUNC
 pricklyPeach.init = function() {
     pricklyPeach.hamburger();
+    // pricklyPeach.scrollTo();
     pricklyPeach.hogPoints();
     pricklyPeach.displayHog();
     pricklyPeach.reset();
